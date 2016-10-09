c4236ff1a9b9d08fb04c2e3f6fac58317f424f3f
Ben197 <jabro888uc@Hotmail.com>
2016-10-09 16:52:46 +0200

    Initiele commit

---
+++ TestGit91okt2016/src/nl/bp/Test22.java
@@ -0,0 +1,16 @@
+package nl.bp;
+
+import nl.bp.HulpFile;
+
+public class Test22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HulpFile hf = new HulpFile();
+		System.out.println("wijziging 1");
+		System.out.println("Uitkomst: " + hf.testDezeFile(1, 3));
+		System.out.println("wijziging 2");
+		
+	}
+
+}
