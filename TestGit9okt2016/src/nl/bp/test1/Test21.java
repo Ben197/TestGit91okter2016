9501c54dc5b78d2c034c58fb48a77730c9d4733e
Ben197 <jabro888uc@Hotmail.com>
2016-10-09 16:17:06 +0200

    Wijziging5a

--- TestGit9okt2016/src/nl/bp/test1/Test21.java
+++ TestGit9okt2016/src/nl/bp/test1/Test21.java
@@ -9,6 +9,7 @@ public class Test21 {
 		System.out.println("Uitkomst: " + hf.testDezeFile(1, 3));
 		System.out.println("wijziging 2");
 		System.out.println("wijziging 4a");
+		System.out.println("wijziging 5a");
 
 	}
 
